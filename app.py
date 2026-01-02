a0ed4f901dc17e6d80ed7fed29f0f4d05de1d301
SonamYadav <ysonam204@gmail.com>
2026-01-02 20:52:17 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import xgboost as xgb
+import numpy as np
+
+app = Flask(__name__)
+CORS(app)  # Allow cross-origin requests
+
+# Load the XGBoost model from JSON format
+model = xgb.XGBRegressor()
+model.load_model("model.json")
+
+@app.route('/')
+def home():
+    return "Calories Prediction API is running!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.get_json()
+
+        # Extract features from the request
+        gender=  float(data['gender'])
+        age = float(data['age'])
+        height = float(data['height'])
+        weight = float(data['weight'])
+        duration= float(data['duration'])
+        heartrate = float(data['heartrate'])
+        bodytemp = float(data['bodytemp'])
+        
+        
+
+        # Prepare the input for prediction
+        features = np.array([[gender,age, height, weight, duration, heartrate, bodytemp]])
+
+        # Make prediction
+        prediction = model.predict(features)[0]
+
+        return jsonify({"prediction": round(float(prediction), 2)})
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=5000,debug=false)
